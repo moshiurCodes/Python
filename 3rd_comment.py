955e40b3f77e489c778728f121cd1025a9ac102e
Moshiur <mdmoshiurcyber24@gmail.com>
2026-01-27 16:46:04 +0600

    Add files via upload

---
+++ 3rd_comment.py
@@ -0,0 +1,16 @@
+
+# moshiur
+
+print("# dara single line comment bujhai")         
+
+'''
+print(10)
+print("moshiur")
+print("my name is moshiur")
+
+'''
+
+print("3 ta single cottation dara multiline comment bujhai")
+
+
+
