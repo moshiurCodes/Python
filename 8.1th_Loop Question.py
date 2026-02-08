15895312696919c4a025643d36d79130aa079214
Moshiur <mdmoshiurcyber24@gmail.com>
2026-02-08 16:35:40 +0600

    Add files via upload

---
+++ 8.1th_Loop Question.py
@@ -0,0 +1,83 @@
+'''
+
+Question 1.
+
+moshiur
+rahman
+moshiur
+rahman
+
+
+Question 2.
+MOSHIUR
+MOSHIUR
+MOSHIUR
+MOSHIUR
+
+Question 3.
+moshiur
+moshiur
+moshiur
+moshiur
+rahman
+
+Question 4.
+
+1
+2
+3
+4
+5
+
+Question 5.
+
+1
+3
+5
+7
+
+Question 6
+
+1
+moshiur
+2
+moshiur
+3
+moshiur
+4
+moshiur
+
+
+Question 7
+
+*
+**
+***
+****
+
+Question 8
+
+1
+12
+123
+1234
+
+Question 9
+
+****
+***
+**
+*
+
+question 10  :  5 সংখ্যা  নামতা (multiplication table) প্রিন্ট করো।
+question 11 : একটি সংখ্যা prime কিনা loop ব্যবহার করে নির্ণয় করো।
+
+
+
+
+
+
+
+
+
+'''
