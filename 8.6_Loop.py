8da11a10a870485578d3f04335fcc3c808801ab6
Moshiur <mdmoshiurcyber24@gmail.com>
2026-02-09 09:28:06 +0600

    Add files via upload

---
+++ 8.6_Loop.py
@@ -0,0 +1,41 @@
+'''
+
+Question 7
+
+*
+**
+***
+****
+
+Question 8
+
+1
+12
+123
+1234
+
+
+'''
+
+
+# Question solve 07
+
+i = 1
+while i <= 4:
+
+
+    print("*" * i)
+    i = i + 1
+
+
+'''
+
+print("*" * i)
+
+"string" * number
+মানে string টা number বার repeat হবে
+
+'''
+
+
+#Question 8:
