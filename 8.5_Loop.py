bffade718c29bbb8258d45fdbf65e6a2c43aa380
Moshiur <mdmoshiurcyber24@gmail.com>
2026-02-09 08:47:14 +0600

    Add files via upload

---
+++ 8.5_Loop.py
@@ -0,0 +1,43 @@
+'''
+Question 5.
+
+1
+3
+5
+7
+
+Question 6
+
+1
+moshiur
+2
+moshiur
+3
+moshiur
+4
+moshiur
+
+'''
+
+# Question 5 solve
+
+i =1
+
+while i<8 :
+
+    print(i)
+
+    i=i+2
+
+
+#Question 6 solve:
+
+
+i=1
+
+while i<5 :
+
+    print(i)
+    print("moshiur")
+
+    i=i+1
